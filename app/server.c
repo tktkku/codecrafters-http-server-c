d9230adc02c5e015c85a4de689e3f54cb231cc21
tktkku <xbqiii@126.com>
2024-03-23 13:09:10 -0700

    pass 6

--- app/server.c
+++ app/server.c
@@ -81,173 +81,176 @@ int main() {
 	
 	 printf("Waiting for a client to connect...\n");
 	 client_addr_len = sizeof(client_addr);
-	
-	int client_fd = 0;
-	client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	if (client_fd <= 0)
+	while (1)
 	{
-		printf("Accept failed: %s \n", strerror(errno));
-		return 1;
-	}
-	 printf("Client connected\n");
-
-	int buffer_size = 2048;
-	char recv_buffer[buffer_size];
-	int recved_size = recv(client_fd, recv_buffer, buffer_size, 0);
-	if (recved_size == -1)
-	{
-		printf("Read error: %s \n", strerror(errno));
-		return -1;
-	}
-	printf("received data=\n=====\n%s\n=====\n", recv_buffer);
-	char* token = strtok(recv_buffer, "\n");
-	// parse header
-	request_header header;
-	while (token != NULL)
-	{
-		printf("token=%s\n", token);
-		if (strncmp(token, "GET", 3) == 0)
+		int client_fd = 0;
+		client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+		if (client_fd <= 0)
 		{
-			int t_len = strlen(token);
-			char* t_copy = (char*)malloc(t_len + 1);
-			strcpy(t_copy, token);
-
-			header.request.type = GET;
-			
-			char* p1 = strchr(t_copy, ' ');
-			char* p2 = strchr(p1 + 1, ' ');
-			char* p3 = strchr(p2 + 1, '\r');
-
-			header.request.path = (char*)malloc(p2 - p1);
-			memset(header.request.path, 0, p2 - p1);
-			strncpy(header.request.path, p1 + 1, p2 - p1 - 1);
-
-			header.request.version = (char*)malloc(p3 - p2);
-			memset(header.request.version, 0, p3 - p2);
-			strncpy(header.request.version, p2 + 1, p3 - p2 - 1);
-
-			free(t_copy);
+			printf("Accept failed: %s \n", strerror(errno));
+			return 1;
 		}
-		else if (strncmp(token, "POST", 4) == 0)
+		printf("Client connected\n");
+
+		int buffer_size = 2048;
+		char recv_buffer[buffer_size];
+		int recved_size = recv(client_fd, recv_buffer, buffer_size, 0);
+		if (recved_size == -1)
 		{
-			int t_len = strlen(token);
-			char* t_copy = (char*)malloc(t_len + 1);
-			strcpy(t_copy, token);
-
-			header.request.type = POST;
-			
-			char* p1 = strchr(t_copy, ' ');
-			char* p2 = strchr(p1 + 1, ' ');
-			char* p3 = strchr(p2 + 1, '\r');
-			
-			header.request.path = (char*)malloc(p2 - p1);
-			memset(header.request.path, 0, p2 - p1);
-			strncpy(header.request.path, p1 + 1, p2 - p1 - 1);
-
-			header.request.version = (char*)malloc(p3 - p2);
-			memset(header.request.version, 0, p3 - p2);
-			strncpy(header.request.version, p2 + 1, p3 - p2 - 1);
-
-			free(t_copy);
+			printf("Read error: %s \n", strerror(errno));
+			return -1;
 		}
-		else if (strncmp(token, "Host:", 5) == 0)
+		printf("received data=\n=====\n%s\n=====\n", recv_buffer);
+		char* token = strtok(recv_buffer, "\n");
+		// parse header
+		request_header header;
+		while (token != NULL)
 		{
-			int t_len = strlen(token);
-			char* t_copy = (char*)malloc(t_len + 1);
-			strcpy(t_copy, token);
-
-			char* p1 = strchr(t_copy, ' ');
-			char* p2 = strchr(p1 + 1, ':');
-			char* p3 = strchr(p2 + 1, '\r');
-
-			header.host.url = (char*)malloc(p2 - p1);
-			memset(header.host.url, 0, p2 - p1);
-			strncpy(header.host.url, p1 + 1, p2 - p1 - 1);
-
-			header.host.port = (char*)malloc(p3 - p2);
-			memset(header.host.port, 0, p3 - p2);
-			strncpy(header.host.port, p2 + 1, p3 - p2 - 1);
-
-			free(t_copy);
-		}
-		else if (strncmp(token, "User-Agent:", 11) == 0)
-		{
-			int t_len = strlen(token);
-			char* t_copy = (char*)malloc(t_len + 1);
-			strcpy(t_copy, token);
-
-			char* p1 = strchr(t_copy, ' ');
-			char* p2 = strchr(p1 + 1, '\r');
-
-			header.user_agent.agent = (char*)malloc(p2 - p1);
-			memset(header.user_agent.agent, 0, p2 - p1);
-			strncpy(header.user_agent.agent, p1 + 1, p2 - p1 - 1);
-			
-			free(t_copy);
-		}
-		token = strtok(NULL, "\n");
-	}
-
-	if (header.request.type == GET)
-	{
-		if (strcmp(header.request.path, "/") == 0)
-		{
-			if (send(client_fd, REQUEST_200, strlen(REQUEST_200), 0) == -1)
+			printf("token=%s\n", token);
+			if (strncmp(token, "GET", 3) == 0)
 			{
-				printf(MSG_SEND_FAILED, strerror(errno));
+				int t_len = strlen(token);
+				char* t_copy = (char*)malloc(t_len + 1);
+				strcpy(t_copy, token);
+
+				header.request.type = GET;
+				
+				char* p1 = strchr(t_copy, ' ');
+				char* p2 = strchr(p1 + 1, ' ');
+				char* p3 = strchr(p2 + 1, '\r');
+
+				header.request.path = (char*)malloc(p2 - p1);
+				memset(header.request.path, 0, p2 - p1);
+				strncpy(header.request.path, p1 + 1, p2 - p1 - 1);
+
+				header.request.version = (char*)malloc(p3 - p2);
+				memset(header.request.version, 0, p3 - p2);
+				strncpy(header.request.version, p2 + 1, p3 - p2 - 1);
+
+				free(t_copy);
 			}
+			else if (strncmp(token, "POST", 4) == 0)
+			{
+				int t_len = strlen(token);
+				char* t_copy = (char*)malloc(t_len + 1);
+				strcpy(t_copy, token);
+
+				header.request.type = POST;
+				
+				char* p1 = strchr(t_copy, ' ');
+				char* p2 = strchr(p1 + 1, ' ');
+				char* p3 = strchr(p2 + 1, '\r');
+				
+				header.request.path = (char*)malloc(p2 - p1);
+				memset(header.request.path, 0, p2 - p1);
+				strncpy(header.request.path, p1 + 1, p2 - p1 - 1);
+
+				header.request.version = (char*)malloc(p3 - p2);
+				memset(header.request.version, 0, p3 - p2);
+				strncpy(header.request.version, p2 + 1, p3 - p2 - 1);
+
+				free(t_copy);
+			}
+			else if (strncmp(token, "Host:", 5) == 0)
+			{
+				int t_len = strlen(token);
+				char* t_copy = (char*)malloc(t_len + 1);
+				strcpy(t_copy, token);
+
+				char* p1 = strchr(t_copy, ' ');
+				char* p2 = strchr(p1 + 1, ':');
+				char* p3 = strchr(p2 + 1, '\r');
+
+				header.host.url = (char*)malloc(p2 - p1);
+				memset(header.host.url, 0, p2 - p1);
+				strncpy(header.host.url, p1 + 1, p2 - p1 - 1);
+
+				header.host.port = (char*)malloc(p3 - p2);
+				memset(header.host.port, 0, p3 - p2);
+				strncpy(header.host.port, p2 + 1, p3 - p2 - 1);
+
+				free(t_copy);
+			}
+			else if (strncmp(token, "User-Agent:", 11) == 0)
+			{
+				int t_len = strlen(token);
+				char* t_copy = (char*)malloc(t_len + 1);
+				strcpy(t_copy, token);
+
+				char* p1 = strchr(t_copy, ' ');
+				char* p2 = strchr(p1 + 1, '\r');
+
+				header.user_agent.agent = (char*)malloc(p2 - p1);
+				memset(header.user_agent.agent, 0, p2 - p1);
+				strncpy(header.user_agent.agent, p1 + 1, p2 - p1 - 1);
+				
+				free(t_copy);
+			}
+			token = strtok(NULL, "\n");
 		}
-		else if (strncmp(header.request.path, "/echo", 5) == 0)
+
+		if (header.request.type == GET)
 		{
-			char* echo = header.request.path + 1;
-			echo = strchr(echo, '/');
-			if (echo == NULL)
-				echo = "";
+			if (strcmp(header.request.path, "/") == 0)
+			{
+				if (send(client_fd, REQUEST_200, strlen(REQUEST_200), 0) == -1)
+				{
+					printf(MSG_SEND_FAILED, strerror(errno));
+				}
+			}
+			else if (strncmp(header.request.path, "/echo", 5) == 0)
+			{
+				char* echo = header.request.path + 1;
+				echo = strchr(echo, '/');
+				if (echo == NULL)
+					echo = "";
+				else
+					echo++;
+				int echo_len = strlen(echo);
+				char *reponse = (char*)malloc(128 + echo_len);
+				sprintf(reponse, 
+				"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+				echo_len, echo);
+				if (send(client_fd, reponse, strlen(reponse), 0) == -1)
+				{
+					printf(MSG_SEND_FAILED, strerror(errno));
+				}
+				free(reponse);
+			}
+			else if (strcmp(header.request.path, "/user-agent") == 0)
+			{
+				int agent_len = strlen(header.user_agent.agent);
+				char* reponse = (char*)malloc(128 + agent_len);
+				sprintf(reponse, 
+				"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+				agent_len, header.user_agent.agent);
+				if (send(client_fd, reponse, strlen(reponse), 0) == -1)
+				{
+					printf(MSG_SEND_FAILED, strerror(errno));
+				}
+				free(reponse);
+			}
 			else
-				echo++;
-			int echo_len = strlen(echo);
-			char *reponse = (char*)malloc(128 + echo_len);
-			sprintf(reponse, 
-			"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-			echo_len, echo);
-			if (send(client_fd, reponse, strlen(reponse), 0) == -1)
 			{
-				printf(MSG_SEND_FAILED, strerror(errno));
+				printf("could not find path: %s \n", header.request.path);
+				if (send(client_fd, REQUEST_404, strlen(REQUEST_404), 0) == -1)
+				{
+					printf(MSG_SEND_FAILED, strerror(errno));
+				}
 			}
-			free(reponse);
 		}
-		else if (strcmp(header.request.path, "/user-agent") == 0)
+		else if (header.request.type == POST)
 		{
-			int agent_len = strlen(header.user_agent.agent);
-			char* reponse = (char*)malloc(128 + agent_len);
-			sprintf(reponse, 
-			"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-			agent_len, header.user_agent.agent);
-			if (send(client_fd, reponse, strlen(reponse), 0) == -1)
-			{
-				printf(MSG_SEND_FAILED, strerror(errno));
-			}
-			free(reponse);
-		}
-		else
-		{
-			printf("could not find path: %s \n", header.request.path);
-			if (send(client_fd, REQUEST_404, strlen(REQUEST_404), 0) == -1)
-			{
-				printf(MSG_SEND_FAILED, strerror(errno));
-			}
-		}
-	}
-	else if (header.request.type == POST)
-	{
 
+		}
+		free(header.request.path);
+		free(header.request.version);
+		free(header.host.url);
+		free(header.host.port);
+		free(header.user_agent.agent);
+		close(client_fd);
 	}
-	free(header.request.path);
-	free(header.request.version);
-	free(header.host.url);
-	free(header.host.port);
-	free(header.user_agent.agent);
-
+	
 	 close(server_fd);
 
 	return 0;
